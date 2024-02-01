b6cc20925c763f1b27993a64fb4ccf79b125d9d8
MaminSiberian <120947225+msiberian42@users.noreply.github.com>
2024-02-01 17:45:33 +0300

    Commit

--- Assets/Game/CameraFacer.cs
+++ Assets/Game/CameraFacer.cs
@@ -1,7 +1,10 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Саммарии
+/// </summary>
 public class CameraFacer : MonoBehaviour
 {
     private Transform target;
